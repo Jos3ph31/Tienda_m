5f8ebe99350179224f82ddde0b74f1eee73b1775
jsilv <jsilv@LAPTOP-V1SGQO4I>
2024-03-27 20:56:42 -0600

    Semana 11, Registro

---
+++ src/main/java/com/tienda_m/service/CorreoService.java
@@ -0,0 +1,12 @@
+package com.tienda_m.service;
+
+import jakarta.mail.MessagingException;
+
+public interface CorreoService {
+    //La firma del método para enviar correos html 
+    public void enviarCorreoHtml(
+            String para,
+            String asunto,
+            String contenidoHtml)
+            throws MessagingException;
+}
